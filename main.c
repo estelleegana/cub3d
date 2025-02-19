f38fef2c25a211864e2ee5bb129b9ca91c8e595a
Estelle van-anh beverley EGANA <estegana@paul-f3Ar3s11.clusters.42paris.fr>
2025-02-19 18:16:27 +0100

    basics

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "cub3d.h"
 
-int main()
+unsigned int valid_map(char **argv)
+{
+    if (ft_strcmp(argv, "ouiii") == 0)
+        return 1;
+    else
+        return 0;
+}
+
+void open_window()
 {
     void *mlxptr;//connexion a la mlx
     void *winptr;//fenetre
@@ -9,3 +17,17 @@ int main()
     winptr = mlx_new_window(mlxptr, WIDTH, HEIGHT, NAME);//creer le fenetre
     mlx_loop(mlxptr);//la garder ouverte
 }
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        printf("program must take a map in format *.cub as a parameter\n");
+    else
+    {
+        if (!valid_map(argv[1]))
+            printf("enter a valid map\n");
+        else
+            open_window();
+    }   
+}  
