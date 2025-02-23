6091a0e5275bb8d6a8042afad41be404294355c6
Estelle van-anh beverley EGANA <estegana@paul-f4Ar2s4.clusters.42paris.fr>
2025-02-23 16:47:55 +0100

    color red

--- cub3d.h
+++ cub3d.h
@@ -3,11 +3,14 @@
 
 # include "libft/libft.h"
 # include "minilibx-linux/mlx.h"
-# include <stdio.h>
+# include <stdio.h>//printf
+# include <fcntl.h>//open
+# include <unistd.h>
 
 # define WIDTH 800
 # define HEIGHT 600
 # define NAME "mon cub3d"
+# define COLOR 0xFF0000
 
 typedef struct s_data
 {
@@ -16,3 +19,10 @@ typedef struct s_data
 }               t_data;
 
 #endif
+
+
+//fleches droite et gauche : vue a droite et gauche de la map
+//W A S D : bouger ds le labyrhinte
+//echap : fermer fenetre et quitter le prog
+//crois rouge : fermer fenetre et quitter le prog
+//./cub3d 
--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include "cub3d.h"
 
-unsigned int valid_map(char *argv)
+void color_window(t_data data)
 {
-    if (ft_strcmp(argv, "ouiii") == 0)
-        return 1;
-    else
-        return 0;
+    int x;
+    int y;
+
+    for (y = 0; y < HEIGHT; y++)
+    {
+        for (x = 0; x < WIDTH; x++)
+            mlx_pixel_put(data.mlx, data.win, x, y, COLOR);
+    }
 }
+
+// void recup_data(int fd)
+// {
+
+// }
+
+// unsigned int valid_scenedescription(int fd)
+// {
+//     if (recup_data(fd))
+//         return 1;
+//     else
+//         return 0;
+// }
 
 int close_window_echap(int touche, t_data *data)
 {
@@ -22,7 +39,7 @@ int close_window_echap(int touche, t_data *data)
 int close_window_cross(int x, int y, t_data *data)
 {
     mlx_destroy_window(data->mlx, data->win);
-    // mlx_destroy_display(data->mlx);
+    mlx_destroy_display(data->mlx);
     exit(0);
 }
 
@@ -30,7 +47,12 @@ void open_window(t_data data)
 {
 
     data.mlx = mlx_init();//connexion a la mlx
+    if (data.mlx == NULL)
+        return;
     data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, NAME);//creer le fenetre
+    if (data.win == NULL)
+        return;
+    // color_window(data);
     mlx_key_hook(data.win, close_window_echap, &data);
     mlx_hook(data.win, 17, 0L, close_window_cross, &data);//17 = fermeture de la fenetre
     mlx_loop(data.mlx);//la garder ouverte
@@ -40,15 +62,22 @@ void open_window(t_data data)
 int main(int argc, char **argv)
 {
     t_data  data;
+    int fd;
 
     if (argc != 2)  
         printf("program must take a map in format *.cub as a parameter\n");
     else
-    {   
-        
-        if (!valid_map(argv[1]))
-            printf("enter a valid map\n");
+    {
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1)
+            perror(argv[1]);
         else
-            open_window(data);
+        {
+            // if (!valid_scenedescription(fd))
+            //     printf("enter a valid map\n");
+            // else
+                open_window(data);
+        }
+        close(fd);
     }   
 }  
